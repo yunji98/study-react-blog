0af058f6aaac70a8d5a69aa8d71d71bbce6fe77d
yunji98 <vkfkddlno@naver.com>
2021-08-30 22:40:18 +0900

    connect mongoDB

--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -11,7 +11,7 @@ const posts = [
 
 // 포스트 작성
 // POST /api/posts {title, body}
-exports.write = (ctx) => {
+export const write = (ctx) => {
   //REST API의 Request Body는 ctx.request.body에서 조회 가능
   const { title, body } = ctx.request.body;
   postId += 1;
@@ -22,13 +22,13 @@ exports.write = (ctx) => {
 
 //포스트 목록 조회
 // GET /api/posts
-exports.list = (ctx) => {
+export const list = (ctx) => {
   ctx.body = posts;
 };
 
 //특정 포스트 조회
 // GET /api/posts/:id
-exports.read = (ctx) => {
+export const read = (ctx) => {
   const { id } = ctx.params;
   /* 주어진 id 값으로 포스트 찾기. 
     파라미터로 받아온 값은 문자열 형식이므로 파라미터를 숫자로 변환하거나 비교할 p.id 값을 문자열로 변경해야 함 */
@@ -46,7 +46,7 @@ exports.read = (ctx) => {
 
 //특정 포스트 제거
 // DELETE /api/posts/:id
-exports.remove = (ctx) => {
+export const remove = (ctx) => {
   const { id } = ctx.params;
   // 해당 id를 가진 posts가 몇번째인지 확인
   const index = posts.findIndex((p) => p.id.toString() === id);
@@ -65,7 +65,7 @@ exports.remove = (ctx) => {
 
 //포스트 수정(교체)
 //PUT /api/posts/:id {title, body}
-exports.replace = (ctx) => {
+export const replace = (ctx) => {
   //PUT 메서드는 전체 포스트 정보를 입력하여 데이터를 통째로 교체할 때 사용
   const { id } = ctx.params;
   const index = posts.findIndex((p) => p.id.toString() === id);
@@ -88,7 +88,7 @@ exports.replace = (ctx) => {
 
 // 포스트 수정(특정 필드 변경)
 // PATCH /api/posts/:id {title, body}
-exports.update = (ctx) => {
+export const update = (ctx) => {
   //PATCH 메서드는 주어진 필드만 교체
   const { id } = ctx.params;
   const index = posts.findIndex((p) => p.id.toString() === id);
--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -1,40 +1,4 @@
-const Koa = require('koa');
-const Router = require('koa-router');
-const bodyParser = require('koa-bodyparser');
+/* eslint-disable no-global-assign */
 
-const api = require('./api');
-
-const app = new Koa();
-const router = new Router();
-
-/*app.use(async (ctx, next) => {
-  console.log(ctx.url);
-  console.log(1);
-  if (ctx.query.authorized !== '1') {
-    ctx.status = 401; // Unauthorized
-    return;
-  }
-  await next();
-  console.log('END');
-});
-
-app.use((ctx, next) => {
-  console.log(2);
-  next();
-});
-
-app.use((ctx) => {
-  ctx.body = 'hello world';
-});*/
-
-router.use('/api', api.routes());
-
-//라우터 적용 이전에 bodyParser 적용!
-app.use(bodyParser());
-
-//app 인스턴스에 라우터 적용
-app.use(router.routes()).use(router.allowedMethods());
-
-app.listen(4000, () => {
-  console.log('Listening to port 4000');
-});
+require = require('esm')(module /*, options */);
+module.exports = require('./main.js');
